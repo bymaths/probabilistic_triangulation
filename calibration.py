dd44cd33d50aaa6b31c02b20f5088c29273a8dba
jiangboyuan <jiangboyuan@kuaishou.com>
2023-09-13 16:01:33 +0800

    updating3

--- calibration.py
+++ calibration.py
@@ -1,9 +1,3 @@
-import torch
-import numpy as np
-import cv2
-from utils import *
-
-
 class CalibrationBatch():
     def __init__(self, points2d, confi2d):
         """
@@ -13,6 +7,7 @@ class CalibrationBatch():
         confi3d : (B,J)
         R : (B,V,3,3)
         t : (B,V,3,1)
+        isdistribution : bool
         """
         self.n_batch,self.n_view,self.n_joint = points2d.shape[:3]
         self.points2d = points2d
@@ -21,6 +16,8 @@ class CalibrationBatch():
         self.confi3d = torch.zeros((self.n_batch,self.n_joint))
         self.R = torch.zeros((self.n_batch,self.n_view,3,3))
         self.t = torch.zeros((self.n_batch,self.n_view,3,1))
+        self.prob_tri = ProbabilisticTriangulation(self.n_batch, self.n_view)
+
 
     def weighted_triangulation(self, points2d, confi2d, R ,t):
         """
@@ -59,6 +56,27 @@ class CalibrationBatch():
 
         return points3d, confi3d
 
+    def weighted_triangulation_sample(self, points2d, confi2d, R ,t):
+        """
+        Args:
+            points2d : (B,V',J,2)
+            confi2d : (B,V',J)
+            R : (M,B, V',3,3)
+            t : (M,B, V',3,1)
+        Returns:
+            sample_points3d : (M,B,J,3)
+            sample_confi3d : (M,B,J)
+        """
+        n_sample = R.shape[0]
+        sample_points3d = torch.zeros((n_sample,self.n_batch,self.n_joint,3))
+        sample_confi3d = torch.zeros((n_sample,self.n_batch,self.n_joint))
+        for i in range(n_sample):
+            for j in range(self.n_batch):
+                sample_points3d[i,j], sample_confi3d = self.weighted_triangulation(
+                    points2d[j], confi2d[j], R[i,j], t[i,j]
+                )
+        return sample_points3d, sample_confi3d
+
     def pnp(self,batch_id):
         for i in range(self.n_view):
             mask = torch.logical_and(self.confi2d[batch_id,i]>0.8,self.confi3d[batch_id]>0.8)
@@ -93,15 +111,25 @@ class CalibrationBatch():
             
             self.pnp(batch_id)
             
-            print(self.R[batch_id,0],self.t[batch_id,0])
-            print(self.mpjpe(2))
-            print(self.confi3d[batch_id])
+            # print(self.R[batch_id,0],self.t[batch_id,0])
+            # print(self.mpjpe(2))
+            # print(self.confi3d[batch_id])
 
             self.points3d[batch_id], self.confi3d[batch_id] = self.weighted_triangulation(
                 self.points2d[batch_id],self.confi2d[batch_id],self.R[batch_id],self.t[batch_id]
             )
-            print(self.confi3d[batch_id])
-            print(self.mpjpe(self.n_view))
+            # print(self.confi3d[batch_id])
+            # print(self.mpjpe(self.n_view))
+
+    def monte_carlo(self):
+        self.eight_point()
+        self.prob_tri.update_paramater_init(self.R,self.t)
+        for i in range(16):
+            sample_R, sample_t = self.prob_tri.sample()
+            sample_points3d, sample_confi3d = self.weighted_triangulation_sample(self.points2d, self.confi2d, sample_R, sample_t)
+            weights = cal_mpjpe_batch(sample_points3d, self.points2d[None],  sample_R, sample_t)
+            self.prob_tri.update_paramater_with_weights(weights)
+            self.R, self.t = self.prob_tri.getRt()
 
     def mpjpe(self, n_view_filter):
         return (homo_to_eulid((self.R[...,:n_view_filter,None,:,:] @ self.points3d[...,None,:,:,None] + self.t[...,:n_view_filter,None,:,:]).squeeze(-1)) - self.points2d[:,:n_view_filter] ).mean()
